7aa936ed761adaf31975bf01d61c07a6b0b863be
George <96864675+gogi1704@users.noreply.github.com>
2022-06-28 23:12:02 +0300

    Create 1.3 Homework

--- app/src/main/java/com/example/nmedia/MainActivity.kt
+++ app/src/main/java/com/example/nmedia/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textContent = findViewById<TextView>(R.id.text_content)
+        textContent.text = "Обратите внимание на блок с количеством просмотров. С ним есть небольшая проблема (поскольку он расположен справа). Если количество просмотров вырастет, например, до 500, то есть целых два варианта:\n" +
+                "\n" +
+                "Отталкиваться от текста (т.е. установить фиксированное расстояние от текста до границы родителя, а саму иконку приклеить к границе текста)\n" +
+                "Оставить достаточное количество места, чтобы уместилось и 500 и 1К, тогда на всех карточках положение этого блока будет одинаковым"
     }
 }
